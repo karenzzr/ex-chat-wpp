966dc762b375890cfcb3999a71222616563978c6
Karen Helena <helenakaren55@gmail.com>
2022-10-26 17:28:13 -0300

    criei apenas uma var mensagens
    adicionei um elemento de lista
    
    criei uma variavel mensagem para saber quem enviou
    
    usei o sitItem para definir os itens, repliquei as mesmas alterações na função de envio 2
    
    por fim criei a função buscarmsg e adicionei um for e um if else

--- chat.js
+++ chat.js
@@ -1,27 +1,72 @@
-var mensagens1 = []
-var mensagens2 = []
+var mensagens = [];
 
 //////////////////////////////////////////////////////////
 
 function envio1() {
-    var msg1 = document.getElementById("mensagemUsuario1").value
+    var msg1 = document.getElementById("mensagemUsuario1");
 
+    var li = document.createElement("li");
+    li.innerText = msg1.value;
+    li.classList.add("mensagemUsuario1");
 
-    document.getElementById("listaDeMensagens").append(msg1)
+    document.getElementById("listaDeMensagens").append(li);
 
-    msg1.push(mensagens1)
-}
+    var mensagem = {
+        textoDaMensagem: msg1.value,
+        quemEnviou: "usuario1"
+    }
+
+    mensagens.push(mensagem);
+    localStorage.setItem("mensagens", JSON.stringify(mensagens));
+
+    msg1.value = "";
+
+};
 
 //////////////////////////////////////////////////////////
 
 function envio2() {
-    var msg2 = document.getElementById("mensagemUsuario2").value;
+    var msg2 = document.getElementById("mensagemUsuario2");
 
     var li = document.createElement("li");
-    li.innerText = msg2;
+    li.innerText = msg2.value;
+    li.classList.add("mensagemUsuario2");
+
     document.getElementById("listaDeMensagens").append(li);
 
+    var mensagem = {
+        textoDaMensagem: msg2.value,
+        quemEnviou: "usuario2"
+    };
 
-    mensagens2.push(msgYoda);
+    mensagens.push(mensagem);
+    localStorage.setItem("mensagens", JSON.stringify(mensagens))
+
+    msg2.value = "";
 }
 
+function buscarmsg() {
+
+    var mensagensJson = localStorage.getItem("mensagens")
+    var mensagens = JSON.parse(mensagensJson);
+
+    var listaDeMensagens = document.getElementById("listaDeMensagens").append(li);
+
+    for (var contador = 0; contador < mensagens.length; contador++) {
+
+        var li = document.createElement("li");
+        li.innerText = msg2.textoDaMensagem;
+
+        if (mensagem.quemenviou === "usuario1") {
+            li.classList.add("mensagemUsuario1")
+
+        } else {
+            li.classList.add("mensagemUsuario2");
+
+        }
+
+        listaDeMensagens.append(li);
+
+    }
+
+}
